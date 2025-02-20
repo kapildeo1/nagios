3b5b8610eadc3dc4d6d9a8155d876a8f681fbde5
kapildeo patil <115640854+kapildeo1@users.noreply.github.com>
2025-02-20 14:47:26 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-11.2.0.linux-amd64.tar.gz
-tar -zxvf grafana-enterprise-8.2.3.linux-amd64.tar.gz
+tar -zxvf grafana-enterprise*.tar.gz
 
 wget gpg.key https://rpm.grafana.com/gpg.key | sudo rpm --import gpg.key
 
