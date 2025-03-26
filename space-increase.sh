d82a528398c9451f491d6158908e9b5276df6fe8
kapildeo patil <115640854+kapildeo1@users.noreply.github.com>
2025-03-26 15:30:41 +0530

    Update space-increase.sh

--- space-increase.sh
+++ space-increase.sh
@@ -1,3 +1,3 @@
 sudo growpart   /dev/xvda 1           
-sudo xfs_growfs -d  /dev/xvda 1 
+sudo xfs_growfs -d  /dev/xvda1    # For the Ext1,2,3 & xfs file type
 sudo resize2fs  /dev/xvda 1       # For the Ext4 file type.
