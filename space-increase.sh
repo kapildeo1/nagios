b511dcae65350af966443cc963a7da3b1ab270e5
kapildeo patil <115640854+kapildeo1@users.noreply.github.com>
2025-03-26 15:34:31 +0530

    Update space-increase.sh

--- space-increase.sh
+++ space-increase.sh
@@ -1,3 +1,3 @@
-sudo growpart   /dev/xvda 1           
-sudo xfs_growfs -d  /dev/xvda1    # For the Ext1,2,3 & xfs file type
-sudo resize2fs  /dev/xvda 1       # For the Ext4 file type.
+sudo growpart   /dev/xvda 1        # increase hard disc space or mounted partition.   
+sudo xfs_growfs -d  /dev/xvda1    # For reflect on the Ext1,2,3 & xfs file type
+sudo resize2fs  /dev/xvda1       # For reflect on the Ext4 file type.
